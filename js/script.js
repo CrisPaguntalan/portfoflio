f05aa9ce8813e051c5ceb4ef268b79ed4ea8a386
CrisPaguntalan <crisestephanpaguntalan@gmail.com>
2022-12-26 17:40:54 +0800

    initial upload

---
+++ js/script.js
@@ -0,0 +1,19 @@
+function gotoHome() {
+    const homePage = document.getElementById("homepage");
+    homePage.scrollIntoView();
+}
+
+function gotoAbout() {
+    const aboutPage = document.getElementById("aboutpage");
+    aboutPage.scrollIntoView();
+}
+
+function gotoProjects() {
+    const projectsPage = document.getElementById("projectspage");
+    projectsPage.scrollIntoView();
+}
+
+function gotoContact() {
+    const contactPage = document.getElementById("contactpage");
+    contactPage.scrollIntoView();
+}
